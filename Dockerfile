c61fa35a3de0f541363a9a58dcf6da5e03f7f9c0
ValentinLagues <valentin.lagues@gmail.com>
2022-11-17 10:21:02 +0100

    docker compose file and datasource fixed

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,18 @@
-FROM node as builder
-WORKDIR /usr/app
-COPY package*.json ./
+FROM node:lts-alpine
+
+RUN apk update && apk add bash git
+RUN mkdir app
+WORKDIR /app
+
+COPY package.json ./
+COPY package-lock.json ./
+
+COPY tsconfig.json ./
+
 RUN npm install
-COPY . .
-RUN npm run build
 
-# stage 2
-FROM node
-WORKDIR /usr/app
-COPY package*.json ./
-RUN npm install --production
+COPY database.postgres ./
 
-COPY --from=builder /usr/app/dist ./dist
+COPY src src
 
-COPY ormconfig.docker.json ./ormconfig.json
-COPY .env .
-
-EXPOSE 4000
-CMD node dist/src/index.js
+CMD ["db:5433", "--" , "npm","start"]
--- src/lib/datasource.ts
+++ src/lib/datasource.ts
@@ -1,16 +1,18 @@
 import {DataSource} from "typeorm";
 import User from "../entity/User";
+import * as dotenv from "dotenv";
 
 import * as path from "path";
+dotenv.config();
 
 export default new DataSource({
   type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "root",
-    password: "admin",
-    database: "test",
-    entities: [User],
-    synchronize: true,
-    logging: false,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT ?+process.env.DATABASE_PORT : 5432 ,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DB,
+  entities: [User],
+  synchronize: true,
+  logging: true
 })
