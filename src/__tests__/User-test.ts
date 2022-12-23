772f02fd86e74b20a5834e3ea69baa8b5d4aee95
ValentinLagues <valentin.lagues@gmail.com>
2022-12-23 17:33:17 +0100

    test and jest config created, test failing for the moment

---
+++ src/__tests__/User-test.ts
@@ -0,0 +1,100 @@
+import { gql, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloServer } from "apollo-server-express";
+import fetch from "cross-fetch";
+  
+  const client = new ApolloServer({
+    link: new HttpLink({
+      uri: "http://localhost:4000/graphql",
+      fetch,
+    }),
+    cache: new InMemoryCache(),
+  });
+  
+  const CREATE_USER = gql`
+    mutation createUser($createUserArgs: MutationCreateUserArgs!) {
+      createUser(createUserArgs: $createUserArgs) {
+        username, 
+        password: hashed, 
+        email, 
+        phone
+      }
+    }
+  `;
+  
+  const LOGIN = gql`
+    query Login($password: String!, $email: String!) {
+      login(password: $password, email: $email) {
+        success
+        token
+      }
+    }
+  `;
+  
+  const LIST_WILDERS = gql`
+    query ListWilders {
+      listWilders {
+        success
+        wilders {
+          email
+          last_name
+          first_name
+          age
+        }
+        message
+      }
+    }
+  `;
+  
+  describe("Wilder resolver", () => {
+    let password = "labite";
+  
+    it("créer wilder", async () => {
+      const res = await client.mutate({
+        mutation: CREATE_USER,
+        variables: {
+          createUserArgs: {
+            username: 'Didier', 
+            password, 
+            email: 'didier@mail.com', 
+            phone: '0558745856'
+          },
+        },
+      });
+  
+      expect(res.data?.createWilder).toEqual({
+        username: 'Didier', 
+        password, 
+        email: 'didier@mail.com', 
+        phone: '0558745856'
+      });
+    });
+    // it("avoir un token si le wilder est correct", async () => {
+    //   const res = await client.query({
+    //     query: LOGIN,
+    //     variables: {
+    //       password,
+    //       email,
+    //     },
+    //     fetchPolicy: "no-cache",
+    //   });
+  
+  
+    //   expect(res.data?.login.success).toBeTruthy();
+    //   expect(res.data?.login.token).toMatch(/^(?:[\w-]*\.){2}[\w-]*$/); //(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)
+    //   token = res.data?.login.token;
+    // });
+    // it("obtenir des informations si le wilder est connecté", async () => {
+    //   const res = await client.query({
+    //     query: LIST_WILDERS,
+    //     fetchPolicy: "no-cache",
+    //     context: {
+    //       headers: {
+    //         authorization: `Bearer ${token}`,
+    //       },
+    //     },
+    //   });
+    
+    //   expect(res.data?.listWilders.success).toBeTruthy();
+    //   expect(res.data?.listWilders.wilders.length).toBeGreaterThan(0);
+    // });
+    });
