0e85d46ce1a57c5a122c698c68e854f57b9db236
ValentinLagues <valentin.lagues@gmail.com>
2023-01-25 16:45:52 +0100

    car resolver and schemas created with all crud mutations

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -2,7 +2,8 @@ import { mergeResolvers } from "@graphql-tools/merge";
 
 import { resolvers as scalarResolvers } from "graphql-scalars";
 import userResolver from "./user.resolver";
+import carResolver from "./car.resolver";
 
-const resolvers = [ userResolver, scalarResolvers];
+const resolvers = [ userResolver, carResolver, scalarResolvers];
 
 export default mergeResolvers(resolvers);
