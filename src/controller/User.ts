62ecc6dbfa5958f851682562ca815a9cc5591d7f
AissiouAbdallahDevFs <aissiou.abdallah@gmail.com>
2023-01-26 11:43:59 +0100

    fix request for login

--- src/controller/User.ts
+++ src/controller/User.ts
@@ -1,7 +1,7 @@
 import { Repository } from "typeorm";
 import datasource from "../lib/datasource";
 import User from "../entity/User";
-import {MutationCreateUserArgs, MutationDeleteUserArgs, MutationUpdateUserArgs} from "@/graphgen"
+import { MutationCreateUserArgs, MutationDeleteUserArgs, MutationUpdateUserArgs } from "@/graphgen"
 
 class UserController {
   db: Repository<User>;
@@ -16,40 +16,47 @@ class UserController {
   async getUser(id: number) {
     return await this.db.findOneBy({ id });
   }
-  async getUserByEmail({email}:{email: string}) {
-    return await this.db.findOne({where:{email}});
+  async getUserByEmail({ email }: { email: string }) {
+    return await this.db.findOne({ where: { email } });
   }
 
   async addUser({ username, password, email, phone, }: MutationCreateUserArgs) {
-      const user = await this.db.save({
-        username,  
-        password, 
-        email, 
-        phone, 
-      });
-      return user;
+    const user = await this.db.save({
+      username,
+      password,
+      email,
+      phone,
+    });
+    return user;
   }
-  async updateUser({ id, username, email, phone }: MutationUpdateUserArgs ) {
-    const user = await this.db.findOne({where:{id: +id}});
-      return await this.db.save({
-        ...user,
-        username,
-        email,
-        phone
-      });
+  async updateUser({ id, username, email, phone }: MutationUpdateUserArgs) {
+    const user = await this.db.findOne({ where: { id: +id } });
+    return await this.db.save({
+      ...user,
+      username,
+      email,
+      phone
+    });
 
-}
-  async deleteUser({ id }: MutationDeleteUserArgs ) {
+  }
+  async deleteUser({ id }: MutationDeleteUserArgs) {
     let msg = "user Introuvable"
-    const user = await this.db.findOne({where:{id: +id}});
-    if(!user){
-      return msg
+    let msg2 = "error request"
+    const user = await this.db.findOne({ where: { id: +id } });
+    if (user) {
+      let result = await this.db.delete(id);
+      console.log(result)
+      if (result?.affected != 0) {
+        return { msg: "user supprimé" }
+      } else {
+        return msg2
+      }
+    } else {
+      console.log('toto')
+      return { msg }
     }
-    return await this.db.remove(user);
-  
 
-    }
-    
+  }
 }
 export default UserController;
 
