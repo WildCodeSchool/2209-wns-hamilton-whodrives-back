deabddaf7ccb2cb37f612d269b1f46143d93825b
AissiouAbdallahDevFs <81308619+AissiouAbdallahDevFs@users.noreply.github.com>
2023-01-31 10:16:41 +0100

    fix userInfo and Add Role CRUD

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -5,6 +5,7 @@ import userResolver from "./user.resolver";
 import carResolver from "./car.resolver";
 import userInfoResolver from "./userInfo.resolver";
 import modelResolver from "./model.resolver";
-const resolvers = [ userResolver, carResolver, scalarResolvers, userInfoResolver,modelResolver];
+import rolesResolver from "./roles.resolver";
+const resolvers = [ userResolver, carResolver, scalarResolvers, userInfoResolver,modelResolver,rolesResolver];
 
 export default mergeResolvers(resolvers);
